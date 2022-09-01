dd4c698159a90f55fda59a48867f1e1724f9167c
rolisangor <vitaliisili@yahoo.com>
2022-09-01 13:57:11 +0200

    FizzBuzz task

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,21 @@
+/*
+* Task:  Print integers 1 to N, but print “Fizz” if an integer is divisible by 3,
+* “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5
+* */
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        int num = 34;
+
+        for(int i = 1; i <= num; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ": FizzBuzz");
+            }else if (i % 3 == 0) {
+                System.out.println(i + ": Fizz");
+            }else if (i % 5 == 0) {
+                System.out.println(i + ": Buzz");
+            }
+        }
+    }
+
+}
