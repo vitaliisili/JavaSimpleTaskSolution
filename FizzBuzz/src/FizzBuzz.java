ef8ecca68ae8e77db4ef7e6e5ae155376d66d24c
rolisangor <vitaliisili@yahoo.com>
2022-09-09 16:15:12 +0200

    FizzBuzz task

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -1,10 +1,13 @@
+import java.math.BigInteger;
+import java.util.*;
+
 /*
  * Task:  Print integers 1 to N, but print “Fizz” if an integer is divisible by 3,
  * “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5
  * */
 public class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int num = 34;
 
         for(int i = 1; i <= num; i++) {
