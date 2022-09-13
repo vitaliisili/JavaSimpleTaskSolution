828f5dbbbc952dbbb04ea8858953e7b5756f62ef
rolisangor <vitaliisili@yahoo.com>
2022-09-13 20:35:11 +0200

    Quick Sort algorithm

--- Quick_Sort_Algorithm/src/QuickSortAlgorithm.java
+++ Quick_Sort_Algorithm/src/QuickSortAlgorithm.java
@@ -8,41 +8,43 @@ import java.util.Arrays;
 public class QuickSortAlgorithm {
 
     public static void main(String[] args) {
-        int[] input = {10, 32, 0, -5, 99, 11, 7, -5, -20, 1, 54, 6, 7, 90, 3, 5, 78, 12, 9, 4, 33, -8, -1, 22, 6, -54};
-        quickSort(input, 0, input.length - 1);
-
+        int[] input = {10, 32, 0, -5, 99, 11, 7, -5,-20, 1, 54,6, 7, 90, 3, 5, 78, 12, 9, 4, 33, -8, -1, 22, 6, -54};
+        quickSort(input);
         System.out.println(Arrays.toString(input));
     }
 
-    private static void quickSort(int[] array, int start, int end) {
-        if (start >= end) {
-            return;
-        }
 
-        int pivot = partition(array, start, end);
-
-        quickSort(array, start, pivot - 1);
-        quickSort(array, pivot + 1, end);
+    public static void quickSort(int[] unsortedArray) {
+        quickSort(unsortedArray, 0, unsortedArray.length - 1);
     }
 
-    public static int partition(int[] array, int start, int end) {
+    private static void  quickSort(int[] unsortedArray, int begin, int end) {
+        if (begin < end) {
+            int pivot = partition(unsortedArray, begin, end);
+
+            quickSort(unsortedArray, begin, pivot - 1);
+            quickSort(unsortedArray, pivot + 1, end);
+        }
+    }
+
+    private static int partition(int[] array, int start, int end) {
         int pivot = array[end];
-        int pIndex = start;
+        int lowerIndex = start;
 
         for (int i = start; i < end; i++) {
             if (array[i] <= pivot) {
                 int temp = array[i];
-                array[i] = array[pIndex];
-                array[pIndex] = temp;
-                pIndex++;
+                array[i] = array[lowerIndex];
+                array[lowerIndex] =temp;
+                lowerIndex++;
             }
         }
 
         int temp = array[end];
-        array[end] = array[pIndex];
-        array[pIndex] = temp;
+        array[end] = array[lowerIndex];
+        array[lowerIndex] = temp;
 
-        return pIndex;
+        return lowerIndex;
     }
 
 }
